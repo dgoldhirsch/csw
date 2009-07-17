63e4058353ff5c5ae449bd3bc893081867e06ec2
unknown <daveg@.(none)>
2009-07-17 00:53:51 -0400

    /fibonacci/show -> /fibonacci/compute

--- app/controllers/fibonacci_controller.rb
+++ app/controllers/fibonacci_controller.rb
@@ -11,24 +11,43 @@ class FibonacciController < ApplicationController
     end
   end
 
-  # GET /fibonacci/:id?algorithm={MATRIX, ADDITION}
-  def show
+  # GET /fibonacci/:n?algorithm={MATRIX, ADDITION}
+  # TODO: Change to POST, because (dimly, theoretically) the computation could have a side-effect within the server.
+  def compute
+    @errors = []
     @n = params[:n]
-    self.set_algorithm_variables
-    puts @algorithm_name
-    @result = CS::fibonacci(@n.to_i, @algorithm_name)
+    self.validate
+
+    if !(@errors.empty?)
+      @result = @errors.to_s
+    elsif
+      @result = CS::fibonacci(@n.to_i, @algorithm_name)
+    end
+
     respond_to do |f|
       f.html
     end
   end
 
-  def set_algorithm_variables
-    if(params[:algorithm] == "addition")
+  def validate
+    #self.validate_post_method  # TODO: Ensure it's truly a POST
+    self.validate_algorithm
+  end
+
+  def validate_post_method
+    @errors << "fibonacci/compute method " + request.method.to_s + " should be POST" if !(request.post?)
+  end
+
+  def validate_algorithm
+    if params[:algorithm] == "addition"
       @algorithm_name = :addition
       @addition_checked = "CHECKED"
-    else
+    else # assume matrix radio button, but return error if something else
       @algorithm_name = :matrix
       @matrix_checked = "CHECKED"
+      if params[:algorithm] != "matrix"
+        @errors << "Unknown algorithm " + params[:algorithm] + " ignored"
+      end
     end
-  end 
+  end
 end
--- test/functional/fibonacci_controller_test.rb
+++ test/functional/fibonacci_controller_test.rb
@@ -1,16 +1,16 @@
 require 'test_helper'
 
 class FibonacciControllerTest < ActionController::TestCase
-  context "New" do
+  context "new" do
     setup do
       get :new
     end
     should_render_template :new
   end
 
-  context "GET/n?algorithm=:matrix" do
+  context "compute matrix" do
     setup do
-      get :show, :n => 6, :algorithm => "matrix"
+      get :compute, :n => 6, :algorithm => "matrix"
     end
     should_assign_to :matrix_checked
     should_not_assign_to :addition_checked
@@ -19,12 +19,12 @@ class FibonacciControllerTest < ActionController::TestCase
       assert_equal :matrix, assigns(:algorithm_name)
       assert_equal 8, assigns(:result)
     end
-    should_render_template :show
+    should_render_template :compute
   end
-  
-  context "GET/n?algorithm=:addition" do
+
+  context "compute addition" do
     setup do
-      get :show, :n => 6, :algorithm => "addition"
+      get :compute, :n => 6, :algorithm => "addition"
     end
     should_not_assign_to :matrix_checked
     should_assign_to :addition_checked
@@ -33,36 +33,57 @@ class FibonacciControllerTest < ActionController::TestCase
       assert_equal :addition, assigns(:algorithm_name)
       assert_equal 8, assigns(:result)
     end
-    should_render_template :show
+    should_render_template :compute
   end
 
-  #context "GET to show" do
+  #context "Must be POST" do
   #  setup do
-  #    get :show, :id => '6'
+  #    get :compute, :n => 6, :algorithm => "matrix"
   #  end
-  #  should "show the answer" do
-  #    assert_select 'p', '8'
-  #  end
-  #  should "show the form" do
-  #    assert_select 'form[action=?][method=post]', fibonaccis_path
-  #  end
-  #end
   #
-  #context "GET to new" do
-  #  setup do
-  #    get :new
-  #  end
-  #  should "show the form" do
-  #    assert_select 'form[action=?][method=post]', fibonaccis_path
+  #  should "fail because of method" do
+  #    # TODO: ensure response is error of GET rather than POST
   #  end
   #end
-  #
-  #context "POST to create" do
-  #  setup do
-  #    post :create, :n => '6', :algorithm => 'matrix'
-  #  end
-  #  should_redirect_to('the show page') do
-  #    fibonacci_path(6, :algorithm => 'matrix')
-  #  end
-  #end
-end
+
+  context "bad parameters" do
+    setup do
+      get :compute, :n => "asdf", :algorithm => "fubar"
+    end
+
+    should "get errors" do
+      # TODO: ensure response contains two error messages, one for n, the other for algorithm
+    end
+  end
+end  
+
+#context "GET to show" do
+#  setup do
+#    get :show, :id => '6'
+#  end
+#  should "show the answer" do
+#    assert_select 'p', '8'
+#  end
+#  should "show the form" do
+#    assert_select 'form[action=?][method=post]', fibonaccis_path
+#  end
+#end
+#
+#context "GET to new" do
+#  setup do
+#    get :new
+#  end
+#  should "show the form" do
+#    assert_select 'form[action=?][method=post]', fibonaccis_path
+#  end
+#end
+#
+#context "POST to create" do
+#  setup do
+#    post :create, :n => '6', :algorithm => 'matrix'
+#  end
+#  should_redirect_to('the show page') do
+#    fibonacci_path(6, :algorithm => 'matrix')
+#  end
+#end
+
